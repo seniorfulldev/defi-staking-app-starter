b14cb478c7c55555354e96dd2ca0690f7af27bcb
seniorfulldev <piaoyilong114@gmail.com>
2022-05-21 16:42:23 +0800

    dapp init

---
+++ test/decentralBank.tests.js
@@ -0,0 +1,54 @@
+const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('decentralBank', ([owner, customer]) => {
+    let tether, rwd, decentralBank
+
+    function tokens(number) {
+        return web3.utils.toWei(number, 'ether')
+    }
+
+    before(async () => {
+        // load contracts
+        tether = await Tether.new()
+        rwd = await RWD.new()
+        decentralBank = await DecentralBank.new(rwd.address, tether.address)
+
+         // Transfer all tokens to DecentralBank( 1 million )
+         await rwd.transfer(decentralBank.address, tokens('1000000'))
+
+         // Transfer 100 mock Tethers to Customer
+         await tether.transfer(customer, tokens('100'), {from: owner})
+    })
+
+    describe('Mock Tether Deployment', async () => {
+        it('matches name successfully', async () => {
+            const name = await tether.name()
+            assert.equal(name, 'Mock Tether Token')
+        })
+    })
+
+    describe('Reward Token', async () => {
+        it('matches name successfully', async () => {
+            const name = await rwd.name()
+            assert.equal(name, 'Reward Token')
+        })
+    })
+
+    describe('Decentral Bank Deployment', async () => {
+        it('matches name successfully', async () => {
+            const name = await decentralBank.name()
+            assert.equal(name, 'DecentralBank')
+        })
+
+        it('contract has tokens', async () => {
+            let balance = await rwd.balanceOf(decentralBank.address)
+            assert.equal(balance, tokens('1000000'))
+        })
+    })
+})
--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ require('babel-polyfill');
 module.exports = {
     networks: {
         development: {
-            host: '127.0.0.1.7545',
+            host: '127.0.0.1',
             port: '7545',
             network_id: '*'
         },
